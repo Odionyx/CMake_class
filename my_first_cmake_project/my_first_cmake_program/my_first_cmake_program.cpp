34aeb2c87587bb851c4cb7d10687355d2ba00de5
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-06-09 16:34:50 +0400

    Initial commit

---
+++ my_first_cmake_project/my_first_cmake_program/my_first_cmake_program.cpp
@@ -0,0 +1,19 @@
+﻿// my_first_cmake_program.cpp: определяет точку входа для приложения.
+//
+
+#include "my_first_cmake_program.h"
+#include <Windows.h>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "ru_RU");
+	SetConsoleCP(1251);
+
+	std::string str;
+	std::cout << "Введите имя: ";
+	std::cin >> str;
+
+	std::cout << "Здравствуйте, " << str << "!" << std::endl;
+	return 0;
+}
